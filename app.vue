fa3c8f55d8d463ecdc5f30d6d852d187d7473082
Claire Peng <clairepeng94@gmail.com>
2025-03-06 22:48:27 +0000

    install tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    test deploy
+  <div class="h-screen w-full flex flex-col bg-purple-950">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+
+  compatibilityDate: '2025-03-06',
 });
