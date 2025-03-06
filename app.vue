7d5f8e0cf5b2d4dd18023deeba9903c185bfe820
Claire Peng <clairepeng94@gmail.com>
2025-03-06 23:01:15 +0000

    add skeleton for data fetching from dummyjson, replace with starling api when ready

--- app.vue
+++ app.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="h-screen w-full flex flex-col bg-purple-950">
+  <div class="h-full w-full flex flex-col bg-purple-950">
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
+    {{ data }}
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+const data = ref(null)
+onMounted(() => {
+  addTodo()
+})
+async function addTodo() {
+  const todo = await $fetch('https://dummyjson.com/products', {
+    method: 'GET',
+  })
+  data.value = todo
+}
+</script>
