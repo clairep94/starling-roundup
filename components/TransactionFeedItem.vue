5cf7388ca70858b907afe8b268986bbe9c983ca7
Claire Peng <clairepeng94@gmail.com>
2025-03-10 18:56:55 +0000

    skeleton transaction feed item

--- components/TransactionFeedItem.vue
+++ components/TransactionFeedItem.vue
@@ -1,6 +1,28 @@
 <template>
-  <div class="flex flex-col justify-between p-2 border-t border-b border-gray-200">
-    {{ amount.currency }}
+  <div class="flex flex-row w-full justify-between p-4 border-t border-b border-gray-200">
+    <div data-test="left-side" class="flex flex-row gap-4 justify-center items-center">
+      <div data-test="logo-container"
+        class="bg-red-500 w-12 h-12 rounded-sm"
+      >
+        {{ spendingCategory }}
+      </div>
+      <div data-test="transaction-details" class="flex flex-col">
+        <div data-test="counter-party-name" class="text-sm font-semibold">
+          {{ counterPartyName }}
+        </div>
+        <div data-test="transaction-time-and-category" class="text-xs text-gray-500">
+          {{ settlementTime }} | {{ spendingCategory }}
+        </div>
+      </div>
+    </div>
+    
+    <!-- AMOUNT & TOGGLE -->
+    <div data-test="right-side" class="flex flex-row gap-4 justify-center items-center">
+      <!-- TODO: icon if round up has already been applied -->
+       ICON
+      <!-- TODO: util function -->
+      {{ amount.currency }} {{ amount.minorUnits }}
+    </div>
   </div>
 </template>
 
@@ -11,6 +33,7 @@ const props = defineProps<{
 }>();
 
 const { amount, direction, settlementTime, counterPartyName, spendingCategory } = props.transactionFeedItem;
+
 </script>
 
 <style scoped>
