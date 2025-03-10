f5b1ac2314ecb1b29c1efd769300343c2c19d8a7
Claire Peng <clairepeng94@gmail.com>
2025-03-10 18:48:19 +0000

    wip transaction feed card

--- components/TransactionFeedItem.vue
+++ components/TransactionFeedItem.vue
@@ -1,7 +1,7 @@
 <template>
-  <pre>
-    {{ transactionFeedItem }}
-  </pre>
+  <div class="flex flex-col justify-between p-2 border-t border-b border-gray-200">
+    {{ amount.currency }}
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,6 +9,8 @@ import type { FeedItem } from "@/types/FeedItem";
 const props = defineProps<{
   transactionFeedItem: FeedItem;
 }>();
+
+const { amount, direction, settlementTime, counterPartyName, spendingCategory } = props.transactionFeedItem;
 </script>
 
 <style scoped>
