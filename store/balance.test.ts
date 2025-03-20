cfc85a221c360902b3dab5d0392ed0eb523447ed
Claire Peng <clairepeng94@gmail.com>
2025-03-20 00:51:25 +0000

    update balance test code

--- store/balance.test.ts
+++ store/balance.test.ts
@@ -28,6 +28,15 @@ describe('Balance Store', () => {
     accountsStore = useAccountsStore();
   });
 
+  const setupValidAuthentication = () => {
+    accountsStore.accounts = [generateMockAccount()];
+    userIdentityStore.token = generateMockToken();
+  };
+
+  const performFetchBalance = async () => {
+    await store.fetchBalance()
+  }
+
   describe('initialization', () => {
     test('should start with a null balance', () => {
       expect(store.balance).toBeNull();
@@ -39,64 +48,78 @@ describe('Balance Store', () => {
   });
 
   describe('fetchBalance', () => {
-    const setupValidAuthentication = () => {
-      accountsStore.accounts = [generateMockAccount()];
-      userIdentityStore.token = generateMockToken();
-    };
+    describe('and when select account or token are missing', async () => {
+      beforeEach(async () => {
+        await performFetchBalance()
+      })
+      test('should not fetch', () => {
+        expect($fetch).not.toHaveBeenCalled();
+      })
+      test('should add an error notification', () => {
+        expect(notificationsStore.addError).toHaveBeenCalledWith(
+          'Cannot fetch balance without an account or token'
+        );
+      })
+    })
 
-    test('should not fetch balance if account or token is missing', async () => {
-      await store.fetchBalance();
+    describe('and while the request is in progress', () => {
+      beforeEach(() => {
+        setupValidAuthentication()
+      })
+      test('should set isLoadingBalance to true when fetching starts', () => {
+        performFetchBalance()
+        expect(store.isLoadingBalance).toBeTruthy();
+      });
+  
+      test('should call the correct endpoint with headers', () => {
+        const { accountUid } = accountsStore.accounts[0];
+        const token = userIdentityStore.token;
+  
+        performFetchBalance()
+  
+        expect($fetch).toHaveBeenCalledWith(
+          `/api/starling/accounts/${accountUid}/balance`,
+          {
+            method: 'GET',
+            headers: { 'session-token': token }
+          }
+        );
+      });
+    })
 
-      expect(notificationsStore.addError).toHaveBeenCalledWith(
-        'Cannot fetch balance without an account or token'
-      );
-      expect($fetch).not.toHaveBeenCalled();
-    });
-
-    test('should set isLoadingBalance to true when fetching starts', () => {
-      setupValidAuthentication();
-      store.fetchBalance();
-      expect(store.isLoadingBalance).toBeTruthy();
-    });
-
-    test('should call the correct endpoint with headers', () => {
-      setupValidAuthentication();
-      const { accountUid } = accountsStore.accounts[0];
-      const token = userIdentityStore.token;
-
-      store.fetchBalance();
-
-      expect($fetch).toHaveBeenCalledWith(
-        `/api/starling/accounts/${accountUid}/balance`,
-        {
-          method: 'GET',
-          headers: { 'session-token': token }
-        }
-      );
-    });
-
-    test('should update balance with fetched data', async () => {
-      setupValidAuthentication();
+    describe('and when the request is successful', async() => {
       const mockBalance = generateMockBalance();
-      $fetch.mockResolvedValue({ data: mockBalance });
-
-      await store.fetchBalance();
-
-      expect(store.balance).toEqual(mockBalance);
-      expect(notificationsStore.addError).not.toHaveBeenCalled();
-    });
-
-    test('should add an error notification when request fails', async () => {
-      setupValidAuthentication();
-      const { accountUid } = accountsStore.accounts[0];
-      const token = userIdentityStore.token;
-
-      const error = generateOfetchError('GET', `/api/starling/accounts/${accountUid}/balance`, 403, 'Forbidden');
-      $fetch.mockRejectedValue(error);
-
-      await store.fetchBalance();
-
-      expect(notificationsStore.addError).toHaveBeenCalledWith(error);
-    });
+      beforeEach( async () => {
+        setupValidAuthentication();
+        $fetch.mockResolvedValue({ data: mockBalance });
+        await performFetchBalance()
+      })
+      test('should update the balance', () => {
+        expect(store.balance).toEqual(mockBalance);
+      })
+      test('should not add an error notification', () => {
+        expect(notificationsStore.addError).not.toHaveBeenCalled();
+      })
+    })
+    describe('and when the test fails', async () => {
+      let accountUid, defaultCategory, error
+      beforeEach(async () => {        
+        setupValidAuthentication()
+        accountUid  = accountsStore.selectedAccount?.accountUid;
+        defaultCategory = accountsStore.selectedAccount?.defaultCategory
+        error = generateOfetchError('GET', `/api/starling/accounts/${accountUid}/balance`, 403, 'Forbidden');
+        $fetch.mockRejectedValue(error)
+        await performFetchBalance()
+      })
+      test('should add an error notification', () => {
+        expect(notificationsStore.addError).toHaveBeenCalledWith(error)
+      })
+      test('should not update the balance', () => {
+        expect(store.balance).toBe(null)
+      })
+      test('should set the loading ref to false', () => {
+        expect(store.isLoadingBalance).toBe(false)
+      })
+    })
   });
 });
