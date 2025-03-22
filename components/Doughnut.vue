7ceffee4e5c678048c7ec90a5ee353e8cea1027c
Claire Peng <clairepeng94@gmail.com>
2025-03-22 23:49:41 +0000

    Wip graphic

--- components/Doughnut.vue
+++ components/Doughnut.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="relative items-center justify-center flex">
-    <div class="absolute bg-none rounded-full h-[164px] w-[164px] flex items-center justify-center translate-y-4 ">
+    <div class="absolute mix-blend-multiply bg-gray-100 rounded-full h-40 w-40 flex flex-col items-center justify-center translate-y-4">
       <slot></slot>
     </div>
     <Doughnut
       v-if="loaded"
       id="my-chart-id"
       :data="chartData"
+      class="opacity-90"
       :options="{ maintainAspectRatio: false, cutout: '70%', radius: '90%' }"
     />
   </div>
@@ -22,25 +23,35 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 const loaded = ref(true);
 
 const chartData = {
-  "labels": ["Savings", "Income", "Payments"],
+  "labels": ["Income", "Payments"],
   "datasets": [
     {
       "label": "Net amount",
-      "data": [300, 50, 100],
+      "data": [50, 100],
       "backgroundColor": [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 205, 86)"
+        "rgba(3,199,194,255)",
+        "rgba(196,39,80,255)",
+        "rgba(52,149,173,255)",
+        "rgba(201,62,133,255)",
+        "rgba(242,133,100,255)",
+        "rgba(0,95,130,255)",
+        "rgba(123,137,64,255)",
+        "rgba(175,175,175,255)"
       ],
       "hoverOffset": 4
     },
     {
       "label": "Transactions",
-      "data": [3, 5,2],
+      "data": [5,2],
       "backgroundColor": [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 205, 86)"
+        "rgba(3,199,194,255)",
+        "rgba(196,39,80,255)",
+        "rgba(52,149,173,255)",
+        "rgba(201,62,133,255)",
+        "rgba(242,133,100,255)",
+        "rgba(0,95,130,255)",
+        "rgba(123,137,64,255)",
+        "rgba(175,175,175,255)"
       ],
       "hoverOffset": 4
     }
--- components/SpendingInsightsByCategory.vue
+++ components/SpendingInsightsByCategory.vue
@@ -1,17 +1,28 @@
 <template>
-  <div v-if="isLoading" class="flex items-center justify-center flex-col">
+  <div v-if="isLoading" class="flex items-center justify-center flex-col min-h-[300px] w-full">
     <pie-spinner variant="secondary"/>
-    <p class="text-lg text-black/70">Loading spending breakdown...</p>
+    <p class="text-md text-black/70">Loading spending breakdown...</p>
   </div>
 
-  <div v-else-if="!insights?.breakdown">
+  <div v-else-if="!insights?.breakdown" class="flex items-center justify-center flex-col min-h-[300px] w-full">
     <p>No breakdown found...</p>
   </div>
 
   <div v-else>
    <Doughnut>
-    <p>Test</p>
+    <h3 class="text-xl font-extrabold text-black/70">
+      {{ insights.direction === 'IN' ? '+' : '' }}
+      {{ formatCurrencyAmount({
+        currency: insights.currency,
+        minorUnits: (insights.netSpend*100)
+      }) }}
+    </h3>
 
+    <h4 data-test="roundup-title" class="text-xs font-semibold text-black/50 text-center">
+      between <br>{{ 
+        new Date(props.dateRange?.summaryStartPeriodInclusive.split('T')[0]).toLocaleDateString('en-GB')
+         }} - {{ new Date(props.dateRange?.summaryEndPeriodExclusive.split('T')[0]).toLocaleDateString('en-GB') }}
+    </h4>
    </Doughnut>
   </div>
 </template>
@@ -21,9 +32,10 @@ import { ref, watch, computed } from 'vue'
 import { useSpendingInsightsStore } from '../store/spendingInsights';
 import '@justeattakeaway/pie-webc/components/spinner.js'
 import Doughnut from './Doughnut.vue';
+import { formatCurrencyAmount } from '../utils/formatData';
 
 const props = defineProps<{
-  dateRange?: {
+  dateRange: {
     summaryStartPeriodInclusive: string,
     summaryEndPeriodExclusive: string
   }
@@ -41,8 +53,5 @@ watch(() => props.dateRange, fetchInsights, { immediate: true, deep: true });
 </script>
 
 <style scoped>
-div {
-  max-width: 400px;
-  max-height: 400px;
-}
+
 </style>
